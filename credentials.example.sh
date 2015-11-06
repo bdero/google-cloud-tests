0cbd708dafef6c7147a10c81ed8458b4f3b38387
Brandon DeRosier <x@bdero.me>
2015-11-05 19:43:03 -0500

    Link to GCE guide in ansible docs

--- credentials.example.sh
+++ credentials.example.sh
@@ -1,8 +1,11 @@
 # To use:
 #   1. Copy this file from `credentials.example.sh` to `credentials.sh`,
 #   2. and replace the sample information below with your real credentials and
-#      project ID.
+#      project ID (see link below for details).
 #   3. Source the file (`source credentials.sh`).
+#
+# See this guideline for obtaining and converting the credentials:
+#   http://docs.ansible.com/ansible/guide_gce.html#credentials
 
 export GCE_EMAIL="my-project-email@developer.gserviceaccount.com"
 export GCE_PROJECT="my-project-id"
