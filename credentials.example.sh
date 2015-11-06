c33c3dac7da8b342f40cb49269db308f69f36d7a
Brandon DeRosier <x@bdero.me>
2015-11-05 19:32:47 -0500

    Use environment vars and pem instead of json

---
+++ credentials.example.sh
@@ -0,0 +1,9 @@
+# To use:
+#   1. Copy this file from `credentials.example.sh` to `credentials.sh`,
+#   2. and replace the sample information below with your real credentials and
+#      project ID.
+#   3. Source the file (`source credentials.sh`).
+
+export GCE_EMAIL="my-project-email@developer.gserviceaccount.com"
+export GCE_PROJECT="my-project-id"
+export GCE_PEM_FILE_PATH="/path/to/my-project-credentials.json"
